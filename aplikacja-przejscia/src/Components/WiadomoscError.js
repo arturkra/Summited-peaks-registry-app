f5e3645a455b85f57486f0327f7de5d9b82e85aa
arturkra <48441700+arturkra@users.noreply.github.com>
2021-12-03 15:40:05 +0100

    Adding Portals

--- aplikacja-przejscia/src/Components/WiadomoscError.js
+++ aplikacja-przejscia/src/Components/WiadomoscError.js
@@ -1,27 +1,36 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import './WiadomoscError.css';
 
+const Backdrop  = (props) => {
+    return <div className='backdrop' onClick = {props.onClick}/>
+}
+
+const Komunikat = (props) => {
+    return <div className = 'komunikat'>
+    <header>
+         Błąd zapisu danych
+     </header>
+     <div className='blad'>
+        <p>
+            {props.typBledu}
+        </p>
+     </div>
+
+    <footer>
+        <button className='przycisk-komunikatu' onClick = {props.onClick} >OK</button>
+    </footer>
+</div>
+}
 
 function WiadomoscError(props) {
 
-    return <div>
-        <div className='backdrop' onClick = {props.onClick}/>
-        <div className = 'komunikat'>
-            <header>
-                 Błąd zapisu danych
-             </header>
-             <div className='blad'>
-                <p>
-                    {props.typBledu}
-                </p>
-             </div>
-
-            <footer>
-                <button className='przycisk-komunikatu' onClick = {props.onClick} >OK</button>
-            </footer>
-        </div>
-   
-    </div>
+    return(
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop onClick={props.onClick}/>, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Komunikat typBledu={props.typBledu} onClick={props.onClick}/>, document.getElementById('overlay-root') )}
+        </React.Fragment>
+    )
 }
 
 export default WiadomoscError;
