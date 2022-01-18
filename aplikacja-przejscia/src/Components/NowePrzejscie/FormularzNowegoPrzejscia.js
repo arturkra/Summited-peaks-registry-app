df8daef9d0aa1e16635667f83dcdf0b9bdf8924d
arturkra <48441700+arturkra@users.noreply.github.com>
2022-01-18 20:00:04 +0100

    useReducer
    Changed state management from useState to useReducer to excersise and better understand the latter.

--- aplikacja-przejscia/src/Components/NowePrzejscie/FormularzNowegoPrzejscia.js
+++ aplikacja-przejscia/src/Components/NowePrzejscie/FormularzNowegoPrzejscia.js
@@ -1,101 +1,207 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useReducer} from "react";
 import './FormularzNowegoPrzejscia.css'
 import WiadomoscError from "../WiadomoscError";
 
+
+const przejscieReducer = (state, action) => {
+
+    if(action.type === 'INPUT_NAZWA') {
+        return {
+            nazwa: action.value,
+            wysokosc: state.wysokosc,
+            kraj: state.kraj,
+            data: state.data,
+            uwagi: state.uwagi,
+            blad: null
+        }
+    }
+    if(action.type === 'INPUT_WYSOKOSC') {
+        return {
+            nazwa: state.nazwa,
+            wysokosc: action.value,
+            kraj: state.kraj,
+            data: state.data,
+            uwagi: state.uwagi,
+            blad: null
+        }
+    }
+    if(action.type === 'INPUT_KRAJ') {
+        return {
+            nazwa: state.nazwa,
+            wysokosc: state.wysokosc,
+            kraj: action.value,
+            data: state.data,
+            uwagi: state.uwagi,
+            blad: null
+        }
+    }
+    if(action.type === 'INPUT_DATA') {
+        return {
+            nazwa: state.nazwa,
+            wysokosc: state.wysokosc,
+            kraj: state.kraj,
+            data: action.value,
+            uwagi: state.uwagi,
+            blad: null
+        }
+    }
+    if(action.type === 'INPUT_UWAGI') {
+        return {
+            nazwa: state.nazwa,
+            wysokosc: state.wysokosc,
+            kraj: state.kraj,
+            data: state.data,
+            uwagi: action.value,
+            blad: null
+        }  
+    }
+    if(action.type === 'TYP_BLEDU') {
+        return {
+            nazwa: state.nazwa,
+            wysokosc: state.wysokosc,
+            kraj: state.kraj,
+            data: state.data,
+            uwagi: state.uwagi,
+            blad: action.value
+        }
+    }
+
+    return {
+        nazwa: '',
+        wysokosc: '',
+        kraj: '',
+        data: '',
+        uwagi: '',
+        blad: null
+    }
+};
+
+// const initialPrzejscieState = {
+//     nazwa: '',
+//     wysokosc: '',
+//     kraj: '',
+//     data: '',
+//     uwagi: '',
+//     blad: null
+// }
+
 function FormularzNowegoPrzejscia(props){
-    const [wprowadzonaNazwa, setNazwa] = useState('');
-    const [wprowadzonaWysokosc, setWysokosc] = useState('');
-    const [wprowadzonyKraj, setKraj] = useState('');
-    const [wprowadzonaData, setData] = useState('');
-    const [wprowadzoneUwagi, setUwagi] = useState('');
-    const [blad, setBlad] = useState()
+    // const [wprowadzonaNazwa, setNazwa] = useState('');
+    // const [wprowadzonaWysokosc, setWysokosc] = useState('');
+    // const [wprowadzonyKraj, setKraj] = useState('');
+    // const [wprowadzonaData, setData] = useState('');
+    // const [wprowadzoneUwagi, setUwagi] = useState('');
+    // const [blad, setBlad] = useState()
+
+    
 
 
+
+    const [przejscieState, dispatchPrzejscie] = useReducer(przejscieReducer,
+        {
+        nazwa: '',
+        wysokosc: '',
+        kraj: '',
+        data: '',
+        uwagi: '',
+        blad: null
+    });
+    
+    
     const nazwaChangeHandler = (event) => {
-        setNazwa(event.target.value);
+        dispatchPrzejscie({type:'INPUT_NAZWA', value: event.target.value});
+        console.log(event.target.value);
+        //setNazwa(event.target.value);
     };
     const wysokoscChangeHandler = (event) => {
-        setWysokosc(event.target.value);
+        dispatchPrzejscie({type:'INPUT_WYSOKOSC', value: event.target.value});
+        //setWysokosc(event.target.value);
     }   
     const krajChangeHandler = (event) => {
-        setKraj(event.target.value);
+        dispatchPrzejscie({type:'INPUT_KRAJ', value: event.target.value});
+        //setKraj(event.target.value);
     }
     const dataChangeHandler = (event) => {
-        setData(event.target.value);
+        dispatchPrzejscie({type:'INPUT_DATA', value: event.target.value});
+        //setData(event.target.value);
     }
     const uwagiChangeHandler = (event) => {
-        setUwagi(event.target.value);
+        dispatchPrzejscie({type:'INPUT_UWAGI', value: event.target.value});
+        //setUwagi(event.target.value);
     }
+
+
 
     const submitHandler = (event) =>{
         event.preventDefault();
-        if(wprowadzonaNazwa.trim().length === 0){
-            setBlad('Podaj nazwę góry');
+        if(przejscieState.nazwa.trim().length === 0){
+            dispatchPrzejscie({type: 'TYP_BLEDU', value:'Podaj nazwę góry'})
+            
+            //setBlad('Podaj nazwę góry');
             return;
             
         }
-        if(wprowadzonaWysokosc.trim().length === 0){
-            setBlad('Podaj wysokość góry');
+        if(przejscieState.wysokosc.trim().length === 0){
+            dispatchPrzejscie({type: 'TYP_BLEDU', value:'Podaj wysokość góry'})
+            //setBlad('Podaj wysokość góry');
             return;
         }
-        if(wprowadzonyKraj.trim().length === 0){
-            setBlad('Podaj kraj');
+        if(przejscieState.kraj.trim().length === 0){
+            dispatchPrzejscie({type: 'TYP_BLEDU', value:'Podaj kraj'})
+            //setBlad('Podaj kraj');
             return;
         }
-        if(wprowadzonaData.trim().length === 0){
-            setBlad('Podaj datę przejścia');
+        if(przejscieState.data.trim().length === 0){
+            dispatchPrzejscie({type: 'TYP_BLEDU', value:'Podaj datę przejścia'})
+            //setBlad('Podaj datę przejścia');
             return;
         }                   
         else{
             const przejscieDane = {
-                nazwaGory:  wprowadzonaNazwa,
-                wysokoscGory: wprowadzonaWysokosc,
-                krajGory: wprowadzonyKraj,
-                dataPrzejscia: new Date(wprowadzonaData),
-                uwagiPrzejscia: wprowadzoneUwagi
+                nazwaGory:  przejscieState.nazwa,
+                wysokoscGory: przejscieState.wysokosc,
+                krajGory: przejscieState.kraj,
+                dataPrzejscia: new Date(przejscieState.data),
+                uwagiPrzejscia: przejscieState.data
             }
             props.onSavePrzejscie(przejscieDane);
-            setData('');
-            setKraj('');
-            setWysokosc('');
-            setNazwa('');
-            setUwagi('');
+            dispatchPrzejscie({type: null});
         }
         
 
     };
 
-    const BladHandler = () =>{
-        setBlad(null);
+     const BladHandler = () =>{
+         dispatchPrzejscie({type:'TYP_BLEDU', value: ''})
     }
 
     return <form onSubmit = {submitHandler}>
         
-        {blad && <WiadomoscError typBledu = {blad} onClick = {BladHandler}/>}
+        {przejscieState.blad && <WiadomoscError typBledu = {przejscieState.blad} onClick = {BladHandler}/>}
         <div className = 'caly-formularz'>
             <div className='naglowek-formularza'>
                 <label>Dodaj Przejscie</label>
             </div>
             <div className='nazwa-gory'>
                 <label>Nazwa Góry</label><br/>
-                <input type='text' value = {wprowadzonaNazwa} onChange ={nazwaChangeHandler}/>
+                <input type='text' value = {przejscieState.nazwa} onChange ={nazwaChangeHandler}/>
             </div>
             <div className = 'wysokosc-gory'>
                 <label>Wysokość Góry</label><br/>
-                <input type='number' value={wprowadzonaWysokosc} min = '0' step = '1' onChange ={wysokoscChangeHandler}/>
+                <input type='number' value={przejscieState.wysokosc} min = '0' step = '1' onChange ={wysokoscChangeHandler}/>
             </div>
             <div className = 'kraj-gory'>
                 <label>Kraj</label><br/>
-                <input type = 'text' value={wprowadzonyKraj} onChange = {krajChangeHandler}/>
+                <input type = 'text' value={przejscieState.kraj} onChange = {krajChangeHandler}/>
             </div>
             
             <div className = 'data-wejscia'>
                 <label>Data</label><br/>
-                <input type = 'date' value = {wprowadzonaData} onChange = {dataChangeHandler}/>
+                <input type = 'date' value = {przejscieState.data} onChange = {dataChangeHandler}/>
             </div>
             <div className = 'uwagi-do-przejscia'>
                 <label>Uwagi</label><br/>
-                <input type = 'text'  value = {wprowadzoneUwagi} onChange = {uwagiChangeHandler}/>
+                <input type = 'text'  value = {przejscieState.uwagi} onChange = {uwagiChangeHandler}/>
             </div>
             <div className = "dodaj-przejscie">
                 <button className='przycisk-dodaj-przejscie' type='submit'>Dodaj przejscie</button>
